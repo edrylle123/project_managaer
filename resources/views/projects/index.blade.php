99ddcdd0e89e99586136f7982bdff8dcbdb96c04
ite19-3c-10@M303.uc-bcf.edu.ph <ite19-3c-10@M303.uc-bcf.edu.ph>
2019-09-17 11:21:12 +0800

    pm

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+@section('content')
+
+	<div class="container">
+		<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">TITLE</th>
+      <th scope="col">DESCRIPTION</th>
+      <th scope="col">DUE DATE</th>
+    </tr>
+  </thead> 
+</table>
+	</div>
+@endsection
